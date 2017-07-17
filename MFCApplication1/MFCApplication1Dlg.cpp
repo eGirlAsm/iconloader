5d1399b0a54928e88c66b48955fd026d6a7e0ce8
egirlasm <egirlasm@gmail.com>
2017-07-17 10:22:45 +0900

    twice commit

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,221 @@
+﻿
+// MFCApplication1Dlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg dialog
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_MESSAGE(WM_SHOWTASK, onShowTask)
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication1Dlg::OnBnClickedButton1)
+	ON_WM_CLOSE()
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg message handlers
+
+LRESULT CMFCApplication1Dlg::onShowTask(WPARAM wParam, LPARAM lParam) //wParam接收的是图标的ID，lParam接收的是鼠标的行为
+{
+	if (wParam != IDR_MAINFRAME)
+		return 1;
+	switch (lParam)
+	{
+	case WM_RBUTTONUP://右键起来时弹出快捷菜单，这里只有一个“关闭”
+	{
+		LPPOINT lpoint = new tagPOINT;
+		::GetCursorPos(lpoint);//得到鼠标位置
+		CMenu menu;
+		menu.CreatePopupMenu();//声明一个弹出式菜单
+							   //增加菜单项“关闭”，点击则发送消息WM_DESTROY给主窗口（已
+							   //隐藏），将程序结束。
+		menu.AppendMenu(MF_STRING, WM_DESTROY, L"exit");
+		//确定弹出式菜单的位置
+		menu.TrackPopupMenu(TPM_LEFTALIGN, lpoint->x, lpoint->y, this);
+		//资源回收
+		HMENU hmenu = menu.Detach();
+		menu.DestroyMenu();
+		delete lpoint;
+	}
+	break;
+	case WM_LBUTTONDBLCLK://双击左键的处理
+	{
+		this->ShowWindow(SW_SHOWNORMAL);//简单的显示主窗口
+	}
+	break;
+	}
+	return 0;
+}
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CMFCApplication1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CMFCApplication1Dlg::ToTray() {
+	m_nid.cbSize = (DWORD)sizeof(NOTIFYICONDATA);
+	m_nid.hWnd = this->m_hWnd;
+	m_nid.uID = IDR_MAINFRAME;
+	m_nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	m_nid.uCallbackMessage = WM_SHOWTASK;
+
+	//自定义的消息名称 WM_SHOWTASK 头函数中定义为WM_USER+1
+	HICON hIco = (HICON)LoadImage(AfxGetResourceHandle(),
+		L"MyIcon.ico", IMAGE_ICON, 32, 32,
+		LR_LOADFROMFILE);
+	m_nid.hIcon = hIco;//LoadIcon(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDR_MAINFRAME));
+	lstrcpy(m_nid.szTip, L"the icon test");//当鼠标放在上面时，所显示的内容
+	Shell_NotifyIcon(NIM_ADD, &m_nid);//在托盘区添加图标
+}
+
+
+void CMFCApplication1Dlg::OnBnClickedButton1()
+{
+	::Shell_NotifyIcon(NIM_DELETE, &m_nid);
+	ToTray();
+	// TODO: Add your control notification handler code here
+
+}
+
+
+void CMFCApplication1Dlg::OnClose()
+{
+	// TODO: Add your message handler code here and/or call default
+	::Shell_NotifyIcon(NIM_DELETE, &m_nid);
+	CDialogEx::OnClose();
+}
---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,38 @@
+
+// MFCApplication1Dlg.h : header file
+//
+
+#pragma once
+
+#define WM_SHOWTASK WM_USER+1
+// CMFCApplication1Dlg dialog
+class CMFCApplication1Dlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCApplication1Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	LRESULT onShowTask(WPARAM wParam, LPARAM lParam);
+
+// Implementation
+protected:
+	HICON m_hIcon;
+	NOTIFYICONDATA m_nid;
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	void ToTray();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnClose();
+};
